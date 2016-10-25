226087ab559723f5f883123d3019db19bd1ead2e
Abdelhak Jemaii <Abdelhak@macbook-pro-de-macbook-3.local>
2016-10-25 01:12:31 +0100

    sync

--- Portfolio-IOS/Portfolio-IOS/Helpers/Extensions/UIViewExtensions.swift
+++ Portfolio-IOS/Portfolio-IOS/Helpers/Extensions/UIViewExtensions.swift
@@ -12,8 +12,6 @@ import UIKit
 // MARK: - Centring and binding a view to its super view
 extension UIView {
     
-    /// Adds constraints to this `UIView` instances `superview` object to make sure this always has the same size as the superview.
-    /// Please note that this has no effect if its `superview` is `nil` – add this `UIView` instance as a subview before calling this.
     func bindFrameToSuperviewBounds() {
         guard let superview = self.superview else {
             print("Error! `superview` was nil – call `addSubview(view: UIView)` before calling `bindFrameToSuperviewBounds()` to fix this.")
